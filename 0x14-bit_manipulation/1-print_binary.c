d7862fb144c3b6a8342e45eae784abc52297818e
boules hisham <bolespolo4@gmail.com>
2023-12-15 18:39:12 -0800

    second task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
-unsigned long int _pow(unsigned int base, unsigned int power) 
+unsigned long int _pow(unsigned int base, unsigned int power)
 {
-
 	unsigned long int num;
 	unsigned int a;
-	num a = 1;
-	for(a = 1; a <= power; a++) 
+
+	num = 1;
+	for (a = 1; a <= power; a++)
 		num *= base;
 	return (num);
-
-
 }
+
