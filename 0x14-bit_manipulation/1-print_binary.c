db985697356468cdad409eaa10cf4f07b96ff67a
boules hisham <bolespolo4@gmail.com>
2023-12-15 18:41:07 -0800

    second task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 unsigned long int _pow(unsigned int base, unsigned int power)
 {
 	unsigned long int num;
@@ -9,5 +10,29 @@ unsigned long int _pow(unsigned int base, unsigned int power)
 	for (a = 1; a <= power; a++)
 		num *= base;
 	return (num);
+}
+
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, check;
+	char flag;
+
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
 }
 
